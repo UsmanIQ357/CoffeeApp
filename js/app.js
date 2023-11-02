887c575a3478139a9530256c45ba8d44f0920e85
UsmanIQ357 <83540087+UsmanIQ357@users.noreply.github.com>
2023-11-02 18:19:59 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,17 +11,8 @@ function getUserMedia(options, successCallback, failureCallback) {
       .catch(failureCallback);
   }
   throw new Error('User Media API not supported.');
-  // otherwise try falling back to old, possibly prefixed API...
-  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
-  if (legacyApi) {
-    // ...and promisify it
-    return new Promise(function (resolve, reject) {
-      legacyApi.bind(navigator)(constraints, resolve, reject);
-    });
-  }
 }
+
 
 // This function is called to start the media stream and recording
 function getStream() {
@@ -48,52 +39,20 @@ function getStream() {
     alert('Error: ' + err);
   });
 }
-
 function setupRecorder(stream) {
-}
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("./serviceWorker.js", { scope: "./" })
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
-  });
-}
-
-// Saves the recording Blob to the cache
-function saveToCache(blob) {
-  if ('caches' in window) {
-    const videoKey = 'my_recorded_video.webm';
-    const request = new Request(videoKey, { mode: 'no-cors' });
-    const response = new Response(blob);
-
-    caches.open('video-cache').then(cache => {
-      cache.put(request, response).then(() => {
-        console.log('Saved video to cache.');
-      }).catch(error => {
-        console.error('Failed to save video to cache:', error);
-      });
-    });
-  } else {
-    console.error('Cache API not supported');
+  try {
+    theRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
+    theRecorder.ondataavailable = function(event) {
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+      }
+    };
+    theRecorder.start(100); // Collect 100ms of data chunks
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    return;
   }
-}
-
-
-// ... Rest of your code such as service worker registration and notifications ...
-
-// Stops the recording and saves the video to cache
-function stopRecordingAndSaveToCache() {
-  console.log('Stopping recording and saving data');
-  theRecorder.stop();
-  theStream.getTracks().forEach(track => track.stop());
-
-  theRecorder.onstop = function() {
-    // Create a Blob from the recorded chunks
-    var blob = new Blob(recordedChunks, { type: 'video/webm' });
-    saveToCache(blob);
-  };
+  console.log('MediaRecorder created');
 }
 
 // This new function retrieves the video from the cache and downloads it.
@@ -131,18 +90,39 @@ async function downloadFromCache() {
   }
 }
 
-function setupRecorder(stream) {
-  try {
-    theRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
-    theRecorder.ondataavailable = function(event) {
-      if (event.data.size > 0) {
-        recordedChunks.push(event.data);
-      }
-    };
-    theRecorder.start(100); // Collect 100ms of data chunks
-  } catch (e) {
-    console.error('Exception while creating MediaRecorder:', e);
-    return;
-  }
-  console.log('MediaRecorder created');
+
+
+// Stops the recording and saves the video to cache
+function stopRecordingAndSaveToCache() {
+  console.log('Stopping recording and saving data');
+  theRecorder.stop();
+  theStream.getTracks().forEach(track => track.stop());
+
+  theRecorder.onstop = function() {
+    // Create a Blob from the recorded chunks
+    var blob = new Blob(recordedChunks, { type: 'video/webm' });
+    saveToCache(blob);
+  };
 }
+
+// Saves the recording Blob to the cache
+function saveToCache(blob) {
+  if ('caches' in window) {
+    const videoKey = 'my_recorded_video.webm';
+    const request = new Request(videoKey, { mode: 'no-cors' });
+    const response = new Response(blob);
+
+    caches.open('video-cache').then(cache => {
+      cache.put(request, response).then(() => {
+        console.log('Saved video to cache.');
+      }).catch(error => {
+        console.error('Failed to save video to cache:', error);
+      });
+    });
+  } else {
+    console.error('Cache API not supported');
+  }
+}
+
+
+// ... Rest of your code such as service worker registration and notifications ...
