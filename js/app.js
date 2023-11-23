d0813ab25ece412676f60e6bcf24a5a4139e40fd
UsmanIQ357 <83540087+UsmanIQ357@users.noreply.github.com>
2023-11-23 17:52:14 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,151 +1,70 @@
-// Define global variables to keep track of the stream and recorder
-var theStream;
-var theRecorder;
-var recordedChunks = [];
-
-// This function initializes user media
-
-
-
-// This function is called to start the media stream and recording
-
-// This new function retrieves the video from the cache and downloads it.
-
-// Stops the recording and saves the video to cache
-
-// Saves the recording Blob to the cache
-var target = document.getElementById('target');
-var watchId;
-
-function appendLocation(location, verb) {
-  verb = verb || 'updated';
-  var newLocation = document.createElement('p');
-  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
-  target.appendChild(newLocation);
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
-if ('storage' in navigator && 'estimate' in navigator.storage) {
-  navigator.storage.estimate()
-    .then(estimate => {
-      document.getElementById('usage').innerHTML = estimate.usage;
-      document.getElementById('quota').innerHTML = estimate.quota;
-      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
-    });
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 
-if ('storage' in navigator && 'persisted' in navigator.storage) {
-  navigator.storage.persisted()
-    .then(persisted => {
-      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
-    });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-function requestPersistence() {
-  if ('storage' in navigator && 'persist' in navigator.storage) {
-    navigator.storage.persist()
-      .then(persisted => {
-        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
-      });
-  }
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
 }
 
-//OFFLINE STORAGE
+//BATTERIESTATUS
 
-if ('localStorage' in window || 'sessionStorage' in window) {
-  var selectedEngine;
-
-  var logTarget = document.getElementById('target');
-  var valueInput = document.getElementById('value');
-
-  var reloadInputValue = function () {
-  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
-    valueInput.value = window[selectedEngine].getItem('myKey') || '';
-  }
-  
-  var selectEngine = function (engine) {
-    selectedEngine = engine;
-    reloadInputValue();
-  };
+if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)) {
+  var target = document.getElementById('target');
 
   function handleChange(change) {
     var timeBadge = new Date().toTimeString().split(' ')[0];
     var newState = document.createElement('p');
     newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    logTarget.appendChild(newState);
-  }
-
-  var radios = document.querySelectorAll('#selectEngine input');
-  for (var i = 0; i < radios.length; ++i) {
-    radios[i].addEventListener('change', function () {
-      selectEngine(this.value)
-    });
+    target.appendChild(newState);
   }
   
-  selectEngine('localStorage');
+  function onChargingChange() {
+    handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
+  }
+  function onChargingTimeChange() {
+    handleChange('Battery charging time changed to ' + this.chargingTime + ' s');
+  }
+  function onDischargingTimeChange() {
+    handleChange('Battery discharging time changed to ' + this.dischargingTime + ' s');
+  }
+  function onLevelChange() {
+    handleChange('Battery level changed to ' + this.level + '');
+  }
 
-  valueInput.addEventListener('keyup', function () {
-    window[selectedEngine].setItem('myKey', this.value);
+  var batteryPromise;
+  
+  if ('getBattery' in navigator) {
+    batteryPromise = navigator.getBattery();
+  } else {
+    batteryPromise = Promise.resolve(navigator.battery);
+  }
+  
+  batteryPromise.then(function (battery) {
+    document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
+    document.getElementById('chargingTime').innerHTML = battery.chargingTime + ' s';
+    document.getElementById('dischargingTime').innerHTML = battery.dischargingTime + ' s';
+    document.getElementById('level').innerHTML = battery.level;
+    
+    battery.addEventListener('chargingchange', onChargingChange);
+    battery.addEventListener('chargingtimechange', onChargingTimeChange);
+    battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
+    battery.addEventListener('levelchange', onLevelChange);
   });
-
-  var onStorageChanged = function (change) {
-    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
-    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
-    if (engine === selectedEngine) {
-      reloadInputValue();
-    }
-  }
-
-  window.addEventListener('storage', onStorageChanged);
 }
-
-//FILE STORAGE
-
-function getReadFile(reader, i) {
-  return function () {
-    var li = document.querySelector('[data-idx="' + i + '"]');
-
-    li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
-  }
-}
-
-function readFiles(files) {
-  document.getElementById('count').innerHTML = files.length;
-
-  var target = document.getElementById('target');
-  target.innerHTML = '';
-
-  for (var i = 0; i < files.length; ++i) {
-    var item = document.createElement('li');
-    item.setAttribute('data-idx', i);
-    var file = files[i];
-
-    var reader = new FileReader();
-    reader.addEventListener('load', getReadFile(reader, i));
-    reader.readAsText(file);
-
-    item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
-    target.appendChild(item);
-  };
-}
-
-async function writeFile() {
-  if (!window.chooseFileSystemEntries) {
-    alert('Native File System API not supported');
-    return;
-  }
-  
-  const target = document.getElementById('target');
-  target.innerHTML = 'Opening file handle...';
-  
-  const handle = await window.chooseFileSystemEntries({
-    type: 'save-file',
-  });
-  
-  const file = await handle.getFile()
-  const writer = await handle.createWriter();
-  await writer.write(0, 'Hello world from What Web Can Do!');
-  await writer.close()
-  
-  target.innerHTML = 'Test content written to ' + file.name + '.';
-}
-// ... Rest of your code such as service worker registration and notifications ...
